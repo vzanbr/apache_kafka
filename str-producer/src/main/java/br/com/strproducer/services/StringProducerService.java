1a479a2c7876a69b9d550d6e33c42baeedd8deb7
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-06 20:16:45 -0300

    fix : Para teste do consumer foi retirado Callback

--- str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
@@ -13,17 +13,19 @@ public class StringProducerService {
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
-                success -> {
-                    if(success != null) {
-                        log.info("Send message with sucess {}", message);
-                        log.info("Partition {} , Offset {}",
-                                success.getRecordMetadata().partition(),
-                                success.getRecordMetadata().offset());
-                    }
-                },
-                error -> log.info("Error send message")
-        );
-
+        log.info("Send Message {}", message);
+        kafkaTemplate.send("str-topic", message);
+//                .addCallback(
+//                success -> {
+//                    if(success != null) {
+//                        log.info("Send message with sucess {}", message);
+//                        log.info("Partition {} , Offset {}",
+//                                success.getRecordMetadata().partition(),
+//                                success.getRecordMetadata().offset());
+//                    }
+//                },
+//                error -> log.info("Error send message")
+//        );
+//
     }
 }
