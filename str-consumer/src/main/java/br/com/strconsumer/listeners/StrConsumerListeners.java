7a6ddf5e1cec108dfa2e23addb64d72a2176a01a
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-06 20:18:41 -0300

    Implementado o consumer , para ocorrer o consumo e a identificação da msg

---
+++ str-consumer/src/main/java/br/com/strconsumer/listeners/StrConsumerListeners.java
@@ -0,0 +1,15 @@
+package br.com.strconsumer.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StrConsumerListeners {
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void listener(String message) {
+       log.info("Recive message {}", message);
+    }
+}
