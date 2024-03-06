95ecf561eaca045d38bcc3fc4bfa902b95967085
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-06 19:49:30 -0300

    feat : implementado ação de calback para retornar menssagem no terminal informando sucesso ou erro, com sua partiçao e offset!

--- str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
@@ -1,9 +1,11 @@
 package br.com.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +13,17 @@ public class StringProducerService {
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Send message with sucess {}", message);
+                        log.info("Partition {} , Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.info("Error send message")
+        );
+
     }
 }
