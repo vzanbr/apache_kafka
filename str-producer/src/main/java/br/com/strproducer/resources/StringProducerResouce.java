2b74c557b24bef5210c861027eb64a9a6c8da6c0
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-06 19:35:46 -0300

    feat: Criando Classe Resource que esta reponsavel de ser o controller

---
+++ str-producer/src/main/java/br/com/strproducer/resources/StringProducerResouce.java
@@ -0,0 +1,21 @@
+package br.com.strproducer.resources;
+
+import br.com.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerResouce {
+
+    private final StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
