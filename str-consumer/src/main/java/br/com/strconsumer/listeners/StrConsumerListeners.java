720063dd1a72e065cf88b86b8910076c4f00e288
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-07 01:00:40 -0300

    feat: Refatorando o codigo para um padrão mais clean, utilizando uma interface para diminuir a linha de codigo!

--- str-consumer/src/main/java/br/com/strconsumer/listeners/StrConsumerListeners.java
+++ str-consumer/src/main/java/br/com/strconsumer/listeners/StrConsumerListeners.java
@@ -1,15 +1,27 @@
 package br.com.strconsumer.listeners;
 
+import br.com.strconsumer.custom.StrConssumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListeners {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-       log.info("Recive message {}", message);
+    @StrConssumerCustomListener(groupId = "group-0")
+    public void create(String message) {
+       log.info("CREATE ::: Recive message {}", message);
+    }
+
+    @StrConssumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Recive message {}", message);
+    }
+
+    @StrConssumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Recive message {}", message);
     }
 }
