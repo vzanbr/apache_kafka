f6a10ad5ed4db6901d1c066e10ab1a154da8b26f
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-07 10:32:29 -0300

    Feat: foi implementado Json consumer

---
+++ json-consumer/src/main/java/br/com/jsonconsumer/listener/JsonListener.java
@@ -0,0 +1,41 @@
+package br.com.jsonconsumer.listener;
+
+import br.com.jsonconsumer.model.Payment;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import static java.lang.Thread.sleep;
+
+@Log4j2
+@Component
+public class JsonListener {
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "create-group", containerFactory = "jsonContainerFactory")
+    public void antiFraud(@Payload Payment payment) {
+        log.info("Recebi o pagamento", payment.toString());
+        sleep(1500);
+        log.info("Validando fraude ...");
+        sleep(2000);
+
+        log.info("Compra Aprovada ...");
+        sleep(3000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "pdf-group", containerFactory = "jsonContainerFactory")
+    public void pdfGenerator(@Payload Payment payment) {
+        log.info("Gerando PDF do produto de id {} ...", payment.getId());
+        sleep(3000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "email-group", containerFactory = "jsonContainerFactory")
+    public void sendEmail() {
+        log.info("Enviando email de confirmação! ...");
+        sleep(4000);
+    }
+}
---
+++ json-consumer/src/main/java/br/com/jsonconsumer/model/Payment.java
@@ -0,0 +1,16 @@
+package br.com.jsonconsumer.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@ToString
+public class Payment implements Serializable {
+
+    private Long id;
+    private Long idUser;
+    private Long idProduct;
+    private String cardNumber;
+}
