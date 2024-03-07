fed562aa309160d16c6e8dd43aedd853b855e2a3
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-07 02:20:14 -0300

    feat: producer config

--- payment-service/src/main/java/br/com/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/br/com/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,27 @@ package br.com.paymentservice.service.impl;
 
 import br.com.paymentservice.model.Payment;
 import br.com.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}" , payment);
+       Thread.sleep(1000);
+
+       log.info("Enviando pagamento...");
+       kafkaTemplate.send("payment-topic", payment);
     }
 }
